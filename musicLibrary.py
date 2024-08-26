1de6bbe78e47c6c583ad003d5a47122c7c4e4473
Ahmad-hubs <ahmeicapture@gmail.com>
2024-08-26 22:39:01 +0500

    Initial commit

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "toba":"https://www.youtube.com/watch?v=uBYCDnRcq5U",
+    "car culture":"https://www.youtube.com/watch?v=5dYirJj0I9M",
+    "taras":"https://www.youtube.com/watch?v=kfXy4W0aD40&list=RDMM&start_radio=1&rv=5dYirJj0I9M",
+    "winning":"https://www.youtube.com/watch?v=vsWxs1tuwDk&list=RDMM&index=18",
+    "power":"https://www.youtube.com/watch?v=jkGj_gS9hj8"
+    }
